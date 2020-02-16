c909a33e4c3b0efb3c26457b1eeca8afa215a4a8
dmoore28 <moorder1112@gmail.com>
2020-02-16 14:54:39 -0600

    Fixed imports in app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WastemapComponent } from './wastemap/wastemap.component';
-import { AboutComponent } from './about/about.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MapComponent } from './map/map.component';
-import { MapFilterComponent } from './map-filter/map-filter.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WastemapComponent } from './pages/wastemap/wastemap.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MapComponent } from './components/map/map.component';
+import { MapFilterComponent } from './components/map-filter/map-filter.component';
 
 @NgModule({
   declarations: [
