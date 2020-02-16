b0f632896397c8ff611f51614c68a81162a36a27
dmoore28 <moorder1112@gmail.com>
2020-02-16 15:20:38 -0600

    Added a basic navbar functionality

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  currentTab: String;
+
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+    this.activeTab();
   }
 
+  activeTab(): void {
+    let url = this.router.url;
+    if (url.includes("home")) {
+      this.currentTab = "home";
+    } else if (url.includes("wastemap")) {
+      this.currentTab = "wastemap";
+    } else {
+      this.currentTab = "about";
+    }
+  }
 }
