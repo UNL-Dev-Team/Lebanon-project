5d7d707320081a8c09cd0e48c02fbcd4506dd7a3
dmoore28 <moorder1112@gmail.com>
2020-02-16 14:38:56 -0600

    Added the basic components and page files.

---
+++ frontend/src/app/pages/wastemap/wastemap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wastemap',
+  templateUrl: './wastemap.component.html',
+  styleUrls: ['./wastemap.component.scss']
+})
+export class WastemapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
